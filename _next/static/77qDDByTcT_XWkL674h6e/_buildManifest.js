1dff51e3ad5edb1891842d781b83cd186c7c8089
Isaac Maamouche <maamouche.isaac@gmail.com>
2022-04-04 15:47:12 +0200

    update dev part

---
+++ _next/static/77qDDByTcT_XWkL674h6e/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/82-1bd0a55a5859f686.js","static/chunks/pages/index-1a3aece8483e5007.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
