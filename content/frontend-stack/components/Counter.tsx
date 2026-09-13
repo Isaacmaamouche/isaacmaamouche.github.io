9897f44887ab9decf4b437b092f77f1fdb85b79d
isaacmaamouche <maamouche.isaac@gmail.com>
2026-09-13 15:53:23 +0200

    fix: tailwind dir discovery

--- content/frontend-stack/components/Counter.tsx
+++ content/frontend-stack/components/Counter.tsx
@@ -6,12 +6,16 @@ export const Counter = () => {
   const [count, setCount] = useState(0);
 
   return (
-    <button
-      className="rounded border border-neutral-300 px-3 py-1 text-sm"
-      onClick={() => setCount((c) => c + 1)}
-      type="button"
-    >
-      Count: {count}
-    </button>
+    <div className="flex flex-col gap-2 items-start">
+      <p>Count: {count}</p>
+
+      <button
+        className="cursor-pointer rounded-lg border border-neutral-300 px-3 py-1 text-sm"
+        onClick={() => setCount((c) => c + 1)}
+        type="button"
+      >
+        increment
+      </button>
+    </div>
   );
 };
